11b9f035ac0c261c2394ba27978f1fe23487eed8
endormi <ernosalo14@gmail.com>
2019-09-27 17:58:56 +0300

    Update .eslintrc.js
    * Update tabWidth
    
    * Remove no-await-in-loop
    
    * Remove import
    
    * Remove func-names
    
    * Remove no-array-index-key
    
    * Remove react/forbid-prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
   "rules": {
     "no-debugger": 0,
     "no-alert": 0,
-    "no-await-in-loop": 0,
     "no-return-assign": ["error", "except-parens"],
     "no-restricted-syntax": [2, "ForInStatement", "LabeledStatement", "WithStatement"],
     "no-unused-vars": [
@@ -50,19 +49,13 @@ module.exports = {
     ],
     "no-console": 0,
     "import/prefer-default-export": 0,
-    "import": 0,
-    "func-names": 0,
     "space-before-function-paren": 0,
     "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
     "no-underscore-dangle": 0,
     "consistent-return": 0,
     "react/display-name": 1,
-    "react/no-array-index-key": 0,
     "react/react-in-jsx-scope": 0,
     "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
     "react/no-unescaped-entities": 0,
     "react/require-default-props": 0,
     "react/jsx-filename-extension": [
@@ -99,7 +92,7 @@ module.exports = {
       "error",
       {
 	"trailingComma": "es5",
-	"tabWidth": 2,
+	"tabWidth": 4,
         "singleQuote": true,
         "printWidth": 70,
       }
